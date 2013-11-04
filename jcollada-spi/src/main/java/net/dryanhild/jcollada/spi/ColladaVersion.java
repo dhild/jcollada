0eaaec5c4f81cf9c02df1d0b4b940e4dbf36e78c
D. Ryan Hild <d.ryan.hild@gmail.com>
2013-11-03 18:31:23 -0700

    Initial commit of spi copy.

---
+++ jcollada-spi/src/main/java/net/dryanhild/jcollada/spi/ColladaVersion.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2013, D. Ryan Hild <d.ryan.hild@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.dryanhild.jcollada.spi;
+
+/**
+ * 
+ * @author D. Ryan Hild <d.ryan.hild@gmail.com>
+ */
+public class ColladaVersion {
+
+    public final int majorVersion;
+    public final int minorVersion;
+    public final int thirdVersion;
+    public final String versionString;
+
+    public ColladaVersion(int major, int minor, int third, String version) {
+        majorVersion = major;
+        minorVersion = minor;
+        thirdVersion = third;
+        versionString = version;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = (61 * hash) + this.majorVersion;
+        hash = (61 * hash) + this.minorVersion;
+        hash = (61 * hash) + this.thirdVersion;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ColladaVersion other = (ColladaVersion) obj;
+        if (this.majorVersion != other.majorVersion) {
+            return false;
+        }
+        if (this.minorVersion != other.minorVersion) {
+            return false;
+        }
+        if (this.thirdVersion != other.thirdVersion) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(20);
+        builder.append("Version=[").append(majorVersion).append('.').append(minorVersion).append('.').append(thirdVersion)
+                .append(", ").append(versionString).append(']');
+        return builder.toString();
+    }
+
+}
