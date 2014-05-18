6d5f7d2a142352617a55f72d582daf5bc8d95ff7
D. Ryan Hild <d.ryan.hild@gmail.com>
2014-05-18 16:53:33 -0600

    Updated license headers.

--- jcollada-spi/src/main/java/net/dryanhild/collada/data/ColladaDocument.java
+++ jcollada-spi/src/main/java/net/dryanhild/collada/data/ColladaDocument.java
@@ -19,6 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package net.dryanhild.collada.data;
 
 import net.dryanhild.collada.VersionSupport;
--- jcollada-spi/src/main/java/net/dryanhild/collada/data/fx/glsl/ShaderProgram.java
+++ jcollada-spi/src/main/java/net/dryanhild/collada/data/fx/glsl/ShaderProgram.java
@@ -19,6 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package net.dryanhild.collada.data.fx.glsl;
 
 import net.dryanhild.collada.data.AddressableType;
